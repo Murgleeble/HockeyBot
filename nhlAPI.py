bc4e23dd18fba3cb10fbeb1a4f38665beb900726
Harbor Wolff <123417166+Murgleeble@users.noreply.github.com>
2023-03-29 17:32:18 -0400

    Update nhlAPI.py

--- nhlAPI.py
+++ nhlAPI.py
@@ -23,22 +23,25 @@ def lines():
           dailyLines = json.loads(jp.read())
 
      for item in dailyLines:
-          name = item['name'].split(' ')
+          try:
+               name = item['name'].split(' ')
 
-          #get the player id
-          for key in contents:
-               if key["name"].lower() == (name[0] + ' ' + name[1]).lower():
-                    ID = key["id"]
+               #get the player id
+               for key in contents:
+                    if key["name"].lower() == (name[0] + ' ' + name[1]).lower():
+                         ID = key["id"]
 
-          API_URL = f"https://statsapi.web.nhl.com/api/v1/people/{ID}"
-          response = requests.get(API_URL + f"/stats?stats=gameLog", params={"Content-Type": "application/json"})
-          data = response.json()
+               API_URL = f"https://statsapi.web.nhl.com/api/v1/people/{ID}"
+               response = requests.get(API_URL + f"/stats?stats=gameLog", params={"Content-Type": "application/json"})
+               data = response.json()
 
-          path = f'{masterPath}/dataS/hockey/22-23'
-          fileName = name[1].lower() + name[0].lower() + ".csv"
-          longPath = path + fileName
+               path = f'{masterPath}/dataS/hockey/22-23'
+               fileName = name[1].lower() + name[0].lower() + ".csv"
+               longPath = path + fileName
 
-          with open(os.path.join(path, fileName), 'w') as fp:
-               for item in data['stats'][0]['splits']:
-                    fp.write(item['date'] + ',' + str(item['stat']['goals']) + ',' + str(item['stat']['assists']) + ',' + str(item['stat']['shots']) + ',' + str(item['stat']['hits']) + ',' + str(item['stat']['blocked']))
-                    fp.write('\n')
+               with open(os.path.join(path, fileName), 'w') as fp:
+                    for item in data['stats'][0]['splits']:
+                              fp.write(item['date'] + ',' + str(item['stat']['goals']) + ',' + str(item['stat']['assists']) + ',' + str(item['stat']['shots']) + ',' + str(item['stat']['hits']) + ',' + str(item['stat']['blocked']))
+                              fp.write('\n')
+          except(KeyError):
+               print("Couldn't parse player: " + name[0] + " " + name[1] + ". \nConsider re-running idCreator to update the list")
